1fba4b1aa74c68e3c432d8742571c3f31096eee5
chiazokam <chiazokamecheta@gmail.com>
2022-08-29 17:57:10 +0100

    chore: set up tests
    - set up eslint
    - set up prettier
    - set up tests

--- src/useTextToSpeech.ts
+++ src/useTextToSpeech.ts
@@ -1,70 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-type UseSpeechSynthesisProps = {
-  onEnd: () => {};
+type UseTextToSpeechProps = {
+  onEnd: () => void
 }
 
 type SpeakProps = {
-  voice: SpeechSynthesisVoice | null;
-  text: string;
-  rate: number;
-  pitch: number;
-  volume: number;
+  voice: SpeechSynthesisVoice | null
+  text: string
+  rate: number
+  pitch: number
+  volume: number
 }
 
-const useSpeechSynthesis = ({ onEnd }: UseSpeechSynthesisProps) => {
-  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
-  const [speaking, setSpeaking] = useState(false);
-  const [supported, setSupported] = useState(false);
+const useTextToSpeech = ({ onEnd }: UseTextToSpeechProps) => {
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([])
+  const [speaking, setSpeaking] = useState(false)
+  const [supported, setSupported] = useState(false)
 
   const getVoices = () => {
     // Firefox seems to have voices upfront and never calls the
     // voiceschanged event
-    let voiceOptions = window.speechSynthesis.getVoices();
+    let voiceOptions = window.speechSynthesis.getVoices()
     if (voiceOptions.length > 0) {
-      setVoices(voiceOptions);
-      return;
+      setVoices(voiceOptions)
+      return
     }
 
     window.speechSynthesis.onvoiceschanged = (event: Event) => {
       const { target } = event
-      voiceOptions = (target as SpeechSynthesis)?.getVoices();
-      setVoices(voiceOptions);
-    };
-  };
+      voiceOptions = (target as SpeechSynthesis)?.getVoices()
+      setVoices(voiceOptions)
+    }
+  }
 
   const handleEnd = () => {
-    setSpeaking(false);
-    onEnd();
-  };
+    setSpeaking(false)
+    onEnd()
+  }
 
   useEffect(() => {
     if (typeof window !== 'undefined' && window.speechSynthesis) {
-      setSupported(true);
-      getVoices();
+      setSupported(true)
+      getVoices()
     }
-  }, []);
+  }, [])
 
   const speak = ({ voice, text = '', rate = 1, pitch = 1, volume = 1 }: SpeakProps) => {
-    if (!supported) return;
-    setSpeaking(true);
+    if (!supported) return
+    setSpeaking(true)
     // Firefox won't repeat an utterance that has been
     // spoken, so we need to create a new instance each time
-    const utterance = new window.SpeechSynthesisUtterance();
-    utterance.text = text;
-    utterance.voice = voice;
-    utterance.onend = handleEnd;
-    utterance.rate = rate;
-    utterance.pitch = pitch;
-    utterance.volume = volume;
-    window.speechSynthesis.speak(utterance);
-  };
+    const utterance = new window.SpeechSynthesisUtterance()
+    utterance.text = text
+    utterance.voice = voice
+    utterance.onend = handleEnd
+    utterance.rate = rate
+    utterance.pitch = pitch
+    utterance.volume = volume
+    window.speechSynthesis.speak(utterance)
+  }
 
   const cancel = () => {
-    if (!supported) return;
-    setSpeaking(false);
-    window.speechSynthesis.cancel();
-  };
+    if (!supported) return
+    setSpeaking(false)
+    window.speechSynthesis.cancel()
+  }
 
   return {
     supported,
@@ -72,7 +72,7 @@ const useSpeechSynthesis = ({ onEnd }: UseSpeechSynthesisProps) => {
     speaking,
     cancel,
     voices,
-  };
-};
+  }
+}
 
-export default useSpeechSynthesis;
+export default useTextToSpeech
